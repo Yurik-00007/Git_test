1e7e64a31ff8b3f17b2fcc94f909b816c4f73ec0
Timofeev Yury <yurik-007@mail.ru>
2024-10-27 03:50:31 +0300

    add hello and list

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,14 @@ function App() {
         <li>2</li>
         <li>3</li>
       </ul>
+      Hello World!
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
     </div>
   );
 }
