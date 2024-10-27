d1adb7219b82adf096a1c8fbbee5ed588f1f4fac
Timofeev Yury <yurik-007@mail.ru>
2024-10-27 03:35:46 +0300

    add list and text

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <li>1</li>
         <li>2</li>
         <li>3</li>
-        <li>4</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, fugiat!</li>
       </ul>
     </div>
   );
